33ddc72241f665e505e2fe9600497c448b2dba97
LindsayPinto <78159928+LindsayPinto@users.noreply.github.com>
2021-05-31 13:04:21 -0500

    Carga, requerimiento 1 y 3 listos

--- App/controller.py
+++ App/controller.py
@@ -23,7 +23,10 @@
 import config as cf
 import model
 import csv
-
+import time
+import tracemalloc
+from DISClib.ADT import list as lt
+from DISClib.ADT import stack as st
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
@@ -41,34 +44,200 @@ def init():
 
 # Funciones para la carga de datos
 
-def loadServices(analyzer, lpsfile, connectionsfile, countriesfile):
+def loadConnections(analyzer, landingPointsFile, connectionsFile, countriesFile):
+    delta_time = -1.0
+    delta_memory = -1.0
+    
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
 
-    lpsfile = cf.data_dir + lpsfile
-    input_file = csv.DictReader(open(lpsfile, encoding="utf-8"), delimiter=",")
-
-    for lp in input_file:
+    landingPointsList = lt.newList('ARRAY_LIST')
+    landingPointsFile = cf.data_dir + landingPointsFile
+    lpFile = csv.DictReader(open(landingPointsFile, encoding="utf-8"), delimiter=",")
+    for lp in lpFile:
+        lt.addLast(landingPointsList, lp)
+        # Por cada landing point en el archivo de entrada, se llama a la función en el modelo
         model.addLandingPoint(analyzer, lp)
 
-    connectionsfile = cf.data_dir + connectionsfile
-    input_file = csv.DictReader(open(connectionsfile, encoding="utf-8"), delimiter=",")
-
-    for connection in input_file:
-        model.addConnection(analyzer, connection)
-
+    countriesList = lt.newList('ARRAY_LIST')
+    countriesFile = cf.data_dir + countriesFile    
+    cntFile = csv.DictReader(open(countriesFile, encoding="utf-8"), delimiter=",")
+    for ctry in cntFile:
+        lt.addLast(countriesList, ctry)
+        # Por cada país en el archivo de entrada, se llama a la función en el modelo
+        model.loadCountry(analyzer, ctry)
     
+    connectionsFile = cf.data_dir + connectionsFile
+    cnnFile = csv.DictReader(open(connectionsFile, encoding="utf-8-sig"), delimiter=",")
+    for cnn in cnnFile:
+        # Por cada conexión en el archivo de entrada, se llama a la función en el modelo
+        model.addConnection(analyzer, cnn)
 
-    # lastservice = None
-    # for service in input_file:
-    #     if lastservice is not None:
-    #         sameservice = lastservice['ServiceNo'] == service['ServiceNo']
-    #         samedirection = lastservice['Direction'] == service['Direction']
-    #         samebusStop = lastservice['BusStopCode'] == service['BusStopCode']
-    #         if sameservice and samedirection and not samebusStop:
-    #             model.addStopConnection(analyzer, lastservice, service)
-    #     lastservice = service
-    # model.addRouteConnections(analyzer)
-    return analyzer
+    # Se crean las conexiónes entre los vertices de cada landing point y entre estos y el vertice de la capital
+    model.addPointConnections(analyzer)
+
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return analyzer, lt.firstElement(landingPointsList), lt.firstElement(countriesList), delta_time, delta_memory
 
 # Funciones de ordenamiento
 
 # Funciones de consulta sobre el catálogo
+
+def totalStops(analyzer):
+    """
+    Cantidad total de puntos de parada
+    """
+    return model.totalStops(analyzer)
+
+def totalConnections(analyzer):
+    """
+    Cantidad total de conexiones
+    """
+    return model.totalConnections(analyzer)
+
+def totalCountries(analyzer):
+    """
+    Cantidad total de países
+    """
+    return model.totalCountries(analyzer)
+
+def findClusters(analyzer, landingPoint1, landingPoint2):
+    delta_time = -1.0
+    delta_memory = -1.0
+    
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
+
+    landingPoint1 = landingPoint1.replace(' ', '').lower()
+    landingPoint2 = landingPoint2.replace(' ', '').lower()
+
+    answer = model.findClusters(analyzer, landingPoint1, landingPoint2)
+
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return analyzer, answer[0], answer[1], delta_time, delta_memory
+
+def findInterLandingPoints(analyzer):
+    delta_time = -1.0
+    delta_memory = -1.0
+    
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
+
+    answer = model.findInterLandingPoints(analyzer)
+
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return analyzer, answer[0], answer[1], delta_time, delta_memory
+
+def findShortestPath(analyzer, pais1, pais2):
+    delta_time = -1.0
+    delta_memory = -1.0
+    
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
+
+    answer = model.findShortestPath(analyzer, pais1, pais2)
+
+    path = []
+
+    while st.size(answer[0]) > 0:
+        step = st.pop(answer[0])
+        path.append({'origin': step['vertexA'], 'destination': step['vertexB'], 'distance' :step['weight']})
+
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return analyzer, path, answer[1], delta_time, delta_memory
+
+def criticalInfrastructure(analyzer):
+    delta_time = -1.0
+    delta_memory = -1.0
+    
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
+
+    answer = model.criticalInfrastructure(analyzer)
+
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return analyzer, answer[0], answer[1], answer[2], delta_time, delta_memory
+
+def failImpact(analyzer, landingPoint):
+    delta_time = -1.0
+    delta_memory = -1.0
+    
+    tracemalloc.start()
+    start_time = getTime()
+    start_memory = getMemory()
+
+    answer = model.failImpact(analyzer, landingPoint)
+
+    stop_memory = getMemory()
+    stop_time = getTime()
+    tracemalloc.stop()
+
+    delta_time = stop_time - start_time
+    delta_memory = deltaMemory(start_memory, stop_memory)
+
+    return analyzer, answer[0], answer[1], delta_time, delta_memory
+
+# Funciones de cálculo de tiempo y memoria
+
+def getTime():
+    """
+    devuelve el instante tiempo de procesamiento en milisegundos
+    """
+    return float(time.perf_counter()*1000)
+
+def getMemory():
+    """
+    toma una muestra de la memoria alocada en instante de tiempo
+    """
+    return tracemalloc.take_snapshot()
+
+
+def deltaMemory(start_memory, stop_memory):
+    """
+    calcula la diferencia en memoria alocada del programa entre dos
+    instantes de tiempo y devuelve el resultado en bytes (ej.: 2100.0 B)
+    """
+    memory_diff = stop_memory.compare_to(start_memory, "filename")
+    delta_memory = 0.0
+
+    # suma de las diferencias en uso de memoria
+    for stat in memory_diff:
+        delta_memory = delta_memory + stat.size_diff
+    # de Byte -> kByte
+    delta_memory = delta_memory/1024.0
+    return delta_memory
--- App/model.py
+++ App/model.py
@@ -32,14 +32,17 @@
 # from DISClib.Algorithms.Sorting import shellsort as sa
 # assert cf
 
-import config
-from DISClib.ADT.graph import gr
-from DISClib.ADT import map as m
+import config as cf
+from DISClib.ADT import graph as gr
+from DISClib.ADT import map as mp
 from DISClib.ADT import list as lt
-from DISClib.Algorithms.Graphs import scc
+from DISClib.DataStructures import mapentry as me
+from DISClib.Algorithms.Graphs import scc as kos
 from DISClib.Algorithms.Graphs import dijsktra as djk
 from DISClib.Utils import error as error
-assert config
+assert cf
+# Se instala Haversine a través del comando "pip install haversine" para calcular distancias en un globo
+import haversine as hs
 
 """
 Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
@@ -58,49 +61,237 @@ def newAnalyzer():
                     'countries': None
                     }
 
-        # analyzer['landingPoints'] = m.newMap(numelements=1280,
-        #                              maptype='PROBING',
-        #                              comparefunction=compareLandingPointsIds)
+        analyzer['landingPoints'] = mp.newMap(numelements=1280,
+                                     maptype='PROBING',
+                                     comparefunction=compareLandingPoints)
+
+        analyzer['landingPointNames'] = mp.newMap(numelements=1280,
+                                     maptype='PROBING',
+                                     comparefunction=compareLandingPointNames)
 
         analyzer['connections'] = gr.newGraph(datastructure='ADJ_LIST',
                                               directed=True,
                                               size=3300,
-                                              comparefunction=compareConnections)
+                                              comparefunction = compareLandingPoints)
+
+        analyzer['countries'] = mp.newMap(numelements=300,
+                                          maptype='PROBING')
+
         return analyzer
     except Exception as exp:
         error.reraise(exp, 'model:newAnalyzer')
 
 # Funciones para agregar informacion al catalogo
 
-def addLandingPoint(analyzer, lp):
+def addLandingPoint(analyzer, landingPoint):
+    # Se obtiene el nombre del landing point, pero se le quitan los espacios y se pasa a minúsculas
+    lpName = landingPoint['name'].replace(' ', '').lower()
+    entry = mp.get(analyzer['landingPoints'], landingPoint['landing_point_id'])
+    if entry is None:
+        # Para cada landing point en el archivo, se crea una entrada en el mapa de landing points
+        entry = newLandingPointEntry(landingPoint)
+        # La entrada se identifica con una llave igual a su columna landing_point_id
+        mp.put(analyzer['landingPoints'], landingPoint['landing_point_id'], entry)
+    # También se crea una entrada en el mapa que relaciona los nombres de los landin points con sus ids (Req 1)
+    mp.put(analyzer['landingPointNames'], lpName, landingPoint['landing_point_id'])
+
+def newLandingPointEntry(landingPoint):
+    # Se obtiene el nombre del país del landing point (-1 es para obtener la última palabra del campo)
+    country = landingPoint['name'].split(',')[-1].strip()
+    # Cada entrada del mapa tiene los datos del archivo junto con una lista de vertices y el país
+    entry = {'landingPointId': landingPoint['landing_point_id'], 
+                'id': landingPoint['id'],
+                'name': landingPoint['name'],
+                'latitude': landingPoint['latitude'],
+                'longitude': landingPoint['longitude'],
+                'country': country, # Sirve para identificar rápidamente el país del landing point
+                'vertices': None} # En este listado se relacionan los vertices que pertenecen al mismo landing point (Mismo landing point, diferente cable)
+    
+    entry['vertices'] = lt.newList('SINGLE_LINKED', compareVertices)
+    return entry
+
+def loadCountry(analyzer, country):
+    entry = mp.get(analyzer['countries'], country['CountryName'])
+    if entry is None:
+        # Para cada país en el archivo, se crea una entrada en el mapa de paises
+        entry = newCountryEntry(country)
+        # La entrada se identifica con una llave igual a su columna con el nombre del país
+        mp.put(analyzer['countries'], country['CountryName'], entry)
+    addPoint(analyzer, country['CapitalName'] + "-" + country['CountryName'])
+
+def newCountryEntry(country):
+    # Cada entrada del mapa tiene los datos del país junto con una referencia al vertice de la capital
+    entry = {'data': country, 
+                'vertex': country['CapitalName'] + "-" + country['CountryName']}
+    return entry
+
+def addPoint(analyzer, pID):
     try:
-        if not gr.containsVertex(analyzer['connections'], lp):
-            gr.insertVertex(analyzer['connections'], lp)
+        # Se intenta crear un vertice. Primero se verifica si existe
+        if gr.containsVertex(analyzer['connections'], pID) == False:
+            gr.insertVertex(analyzer['connections'], pID)
         return analyzer
     except Exception as exp:
-        error.reraise(exp, 'model:addLandingPoint')
+        error.reraise(exp, 'model:addPoint')
 
-def addConnection(analyzer, connection):
+def addConnection(analyzer, cnn):
+    # Para cada conexión, se crea un vertice con el landing point de origen y el cable correspondiente
+    originPoint = cnn["origin"] + '-' + cnn["cable_name"]
+    # Se obtiene la entrada del mapa de landing point correspondiente al landing point de origen
+    originLP = me.getValue(mp.get(analyzer['landingPoints'], cnn['origin']))
+    # Si el vertice que relaciona al landing point con el cable no existe, se crea
+    if lt.isPresent(originLP['vertices'], originPoint) == 0:
+        lt.addLast(originLP['vertices'], originPoint)
+    # Se añade el vertice al grafo
+    addPoint(analyzer, originPoint)
 
-    origin = gr.vertices(analyzer["connections"])[connection["origin"]]
-    destination = gr.vertices(analyzer["connections"])[connection["destination"]]
+    # Se crea un vertice con el landing point de destino y el cable correspondiente
+    destinationPoint = cnn["destination"] + '-' + cnn["cable_name"]
+    # Se obtiene la entrada del mapa de landing point correspondiente al landing point de destino
+    destinationLP = me.getValue(mp.get(analyzer['landingPoints'], cnn['destination']))
+    # Si el vertice que relaciona al landing point con el cable no existe, se crea
+    if lt.isPresent(destinationLP['vertices'], destinationPoint) == 0:
+        lt.addLast(destinationLP['vertices'], destinationPoint)
+    # Se añade el vertice al grafo
+    addPoint(analyzer, destinationPoint)
 
-    edge = gr.getEdge(analyzer['connections'], origin, destination)
+    # Se determina la distancia (peso) del arco
+    if cnn["cable_length"] != "n.a.":
+        length = float(cnn["cable_length"].replace(",", "").split()[0])
+    else:
+        # Si la distancia no se da en el archivo de entrada, se calcula a partir de las coordenadas de los landing points
+        origin = (float(originLP["latitude"]), float(originLP["longitude"]))
+        destination = (float(destinationLP["latitude"]), float(destinationLP["longitude"]))
+        length = round(hs.haversine(origin, destination), 2)
+    # Se añade el arco al grafo
+    addEdge(analyzer, originPoint, destinationPoint, length)
+
+def addEdge(analyzer, originPoint, destinationPoint, distance):
+    edge = gr.getEdge(analyzer['connections'], originPoint, destinationPoint)
+    # Se intenta crear un arco. Primero se verifica si existe
     if edge is None:
-        gr.addEdge(analyzer['connections'], origin, destination, connection["cable_length"])
+        gr.addEdge(analyzer['connections'], originPoint, destinationPoint, distance)
     return analyzer
+
+def addPointConnections(analyzer):
+    # Para cada landing point es necesario realizar dos acciones:
+    # 1. Crear arcos entre los vertices correspondientes al landing point, con un peso mínimo
+    # 2. Crear arcos entre los vertices del landing point y el vertice correspondiente a la capital del país
+    for lp in lt.iterator(mp.valueSet(analyzer['landingPoints'])):
+        vertices = lp['vertices']
+        originIndex = lt.size(vertices)
+        
+        # Se obtiene el vertice de la capital del país
+        country = me.getValue(mp.get(analyzer['countries'], lp['country']))
+        countryVertex = country['vertex']
+
+        # Se calcula la distancia del landing point a la capital, mediante coordenadas
+        origin = (float(lp["latitude"]), float(lp["longitude"]))
+        destination = (float(country['data']["CapitalLatitude"]), float(country['data']["CapitalLongitude"]))
+        haversineLength = round(hs.haversine(origin, destination), 2)
+
+        # Para cada vertice del landing point, se crean los arcos correspondientes
+        for vertex in lt.iterator(vertices):
+            addEdge(analyzer, vertex, countryVertex, haversineLength)
+            addEdge(analyzer, countryVertex, vertex, haversineLength)
+
+        # Si el landing point solo tiene un vertice (solo se conecta a un cable) no es necesario crear más arcos
+        if originIndex <= 1:
+            continue
+
+        # Si el landing point tiene más de un vertice, se crean arcos entre ellos. La distancia se considera mínima
+        for i in range(1, lt.size(vertices)):
+            originVertex = lt.getElement(vertices, originIndex)
+            destinationVertex = lt.getElement(vertices, i)
+            addEdge(analyzer, originVertex, destinationVertex, 0.1)
+            addEdge(analyzer, destinationVertex, originVertex, 0.1)
+            originIndex = i
 
 # Funciones para creacion de datos
 
 # Funciones de consulta
 
+def totalStops(analyzer):
+    """
+    Retorna la cantidad total de vertices del grafo
+    """
+    return gr.numVertices(analyzer['connections'])
+
+def totalConnections(analyzer):
+    """
+    Retorna la cantidad total de arcos del grafo
+    """
+    return gr.numEdges(analyzer['connections'])
+
+def totalCountries(analyzer):
+    """
+    Retorna la cantidad total de países
+    """
+    return mp.size(analyzer['countries'])
+
 def formatVertex(landingPoint):
     vertexId = landingPoint['landing_point_id']
     return vertexId
 
+def findClusters(analyzer, landingPoint1, landingPoint2):
+    #Se ejecuta el algotimo de Kosaraju
+    scc = kos.KosarajuSCC(analyzer['connections'])
+
+    #Se obtienen los ids de los landing points a partir de sus nombres
+    vertexAId = me.getValue(mp.get(analyzer['landingPointNames'], landingPoint1))
+    vertexBId = me.getValue(mp.get(analyzer['landingPointNames'], landingPoint2))
+
+    # Se obtiene la entrada del mapa para cada uno de los landing points
+    lpA = me.getValue(mp.get(analyzer['landingPoints'], vertexAId))
+    lpB = me.getValue(mp.get(analyzer['landingPoints'], vertexBId))
+
+    # Se obtiene el primer vertice de cada uno de los landing points
+    vertexA = lt.getElement(lpA['vertices'], 1)
+    vertexB = lt.getElement(lpB['vertices'], 1)
+
+    # Se retornan los valores devueltos por las funciones de componentes conectados y vertices fuertemente conectados    
+    return kos.connectedComponents(scc), kos.stronglyConnected(scc, vertexA, vertexB)
+
+def findInterLandingPoints(analyzer):
+    return [], 0
+
+def findShortestPath(analyzer, pais1, pais2):
+    # Se obtiene el vertice correspondiente al primer país
+    country1 = me.getValue(mp.get(analyzer['countries'], pais1))
+    vertex1 = country1['vertex']
+
+    # Se obtiene el vertice correspondiente al segundo país
+    country2 = me.getValue(mp.get(analyzer['countries'], pais2))
+    vertex2 = country2['vertex']
+
+    # Se ejecuta el algoritmo de Dijkstra
+    search = djk.Dijkstra(analyzer['connections'], vertex1)
+
+    # Se obtiene la distancia y el camino
+    dist = djk.distTo(search, vertex2)
+    path = djk.pathTo(search, vertex2)
+
+    return path, dist
+
+def criticalInfrastructure(analyzer):
+    return 0, 0, 0
+
+def failImpact(analyzer, landingPoint):
+    return 0, []
+
 # Funciones utilizadas para comparar elementos dentro de una lista
 
-def compareLandingPointsIds(landingPoint, keyvalueLandingPoint):
+# def compareLandingPointsIds(landingPoint, keyvalueLandingPoint):
+
+#     landingPointCode = keyvalueLandingPoint['key']
+#     if (landingPoint == landingPointCode):
+#         return 0
+#     elif (landingPoint > landingPointCode):
+#         return 1
+#     else:
+#         return -1
+
+def compareLandingPoints(landingPoint, keyvalueLandingPoint):
 
     landingPointCode = keyvalueLandingPoint['key']
     if (landingPoint == landingPointCode):
@@ -110,14 +301,27 @@ def compareLandingPointsIds(landingPoint, keyvalueLandingPoint):
     else:
         return -1
 
+# def compareConnections(firstCnn, secondCnn):
 
-def compareConnections(connection1, connnection2):
+#     if firstCnn == secondCnn:
+#         return 0
+#     elif firstCnn > secondCnn:
+#         return 1
+#     else:
+#         return -1
 
-    if (connection1["origin"] == connnection2["origin"]):
-        if(connection1["destination"] == connnection2["destination"]):
-            if(connection1["cable_id"] == connnection2["cable_id"]):
-                return 0
-    
-    return -1
+def compareLandingPointNames(landingPoint1, landingPoint):
+
+    landingPoint2 = landingPoint['key']
+    if (landingPoint1 == landingPoint2):
+        return 0
+    else:
+        return 1
+
+def compareVertices(vertex1, vertex2):
+    if(vertex1 == vertex2):
+        return 0
+    else:
+        return 1
 
 # Funciones de ordenamiento
